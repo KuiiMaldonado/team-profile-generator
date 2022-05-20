4ebd1e9d066383943b6b8036465a3a8fdbdf2cd6
Cuitlahuac Maldonado <cuitlahuacjr@gmail.com>
2022-05-20 13:41:20 -0500

    Exporting classes and asking for input async

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Manager = require('./manager');
+const Engineer = require('./engineer');
+const Intern = require('./intern');
 
 const inputIsNaN = (x) => {
     if(isNaN(x)) {
@@ -9,22 +12,23 @@ const inputIsNaN = (x) => {
         return true;
 }
 
-function getManagerInfo(){
-    inquirer.prompt([
+async function getEmployeeInfo(isManager, isEngineer, isIntern){
+
+    const result = await inquirer.prompt([
         {
             type: "input",
-            message: "What's the team manager's name?",
+            message: "What's the employee name?",
             name:'name',
         },
         {
             type: "input",
-            message: "What's the team manager's id?",
+            message: "What's the employee id?",
             name:'id',
             validate: inputIsNaN,
         },
         {
             type: "input",
-            message: "What's the team manager's email?",
+            message: "What's the employee email?",
             name:'email',
         },
         {
@@ -32,10 +36,35 @@ function getManagerInfo(){
             message: "What's the team manager's office number?",
             name:'officeNumber',
             validate: inputIsNaN,
+            when: isManager,
         },
-    ]).then(response => {
-        console.log(response);
-    });
+        {
+            type: "input",
+            message: "What's the engineer github",
+            name:'github',
+            when: isEngineer,
+        },
+        {
+            type: "input",
+            message: "What's the intern's school",
+            name:'school',
+            when: isIntern,
+        },
+    ]);
+    console.log('Termina async');
+    return result;
 }
 
-getManagerInfo();
+async function getManagerInfo() {
+    const employeeInfo = await getEmployeeInfo(true, false, false);
+    return employeeInfo;
+}
+
+async function init() {
+    const managerInfo = await getManagerInfo();
+    const {name, id, email, officeNumber} = managerInfo;
+    const manager = new Manager(name, id, email, officeNumber);
+    console.log(manager);
+}
+
+init();
